cfd08cd8ace3bfa7453f547e029b3b9a42ac39cc
Félix <71280649+FelixCalvo@users.noreply.github.com>
2023-05-26 13:04:03 +0200

    Final Conexion server add, save, remove

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, //si esta vacia la ruta como es la primera vez redirecciona a dashboard
+  //El :carácter de dos puntos en pathindica que :ides un marcador de posición para un héroe específico id.
+  { path: 'detail/:id', component: HeroDetailComponent },//usar la id del heroe para usarla parra la url y ver la vist de ese heroe
 ];
 
 @NgModule({
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -10,6 +15,35 @@ export class HeroDetailComponent {
 //@Input() es un decorador de Angular que indica que la propiedad hero es una entrada de datos que será proporcionada por un componente padre. 
 //en este caso es cuando se clica un boton (heroes.component.html)
   @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {//efecto boton retroceso
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
+  
 }
 
 
